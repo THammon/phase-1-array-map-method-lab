62d1b208e0773ae1011c5389a11192c3302cfe53
Tristen <tristen.hammon21@gmail.com>
2022-08-31 15:01:32 -0500

    commit tree e00b5ddd6da7904fec6b77d66ec18cedd7146722

--- index.js
+++ index.js
@@ -11,6 +11,45 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const cased = tutorials.map((cb) => {
+    const newCase = cb
+      .split(' ')
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      })
+      .join(' ')
+    return newCase
+  })
+  return cased  
 }
+
+// const arr = str.split('');
+
+// for (var i = 0; i < arr.length; i++) {
+//   arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+// }
+
+// const tutorials2 = arr.join('');
+// console.log(tutorials2);
+
+// function titleCased() {
+//   return tutorials.map(element => {
+//     return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+//   })
+// }
+
+// newArr[i] = newArr[i].charAt(0).toUpperCase() + newArr[i].substr(1);
+
+//Working Function but confusing (below)
+
+// function titleCased(){
+//   const capTutorials = tutorials.map(t => {
+//     const words = t.split(" ").map(w => {
+//       return w.charAt(0).toUpperCase() + w.slice(1);
+//     }).join(" ");
+//     console.log(words);
+//     return words; 
+//   })
+//   return capTutorials;
+// }
